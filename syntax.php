67556687b14e050f970b5fb58d05b23c666e92c5
Christophe Drevet-Droguet <cdrevet@atac.fr>
2013-10-09 14:54:00 +0200

    update date

--- syntax.php
+++ syntax.php
@@ -25,7 +25,7 @@ class syntax_plugin_cellbg extends DokuWiki_Syntax_Plugin {
         return array(
             'author' => 'dr4Ke',
             'email'  => 'dr4ke@dr4ke.net',
-            'date'   => '2009-12-08',
+            'date'   => '2013-10-09',
             'name'   => 'Cells background color plugin',
             'desc'   => 'Sets background of a cell with a specific color',
             'url'    => 'http://www.dokuwiki.org/plugin:cellbg',
